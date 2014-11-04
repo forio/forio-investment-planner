bddefd2d5d1de4a3fb808a31e6557586231cbb05
Dan Baker <dbaker@forio.com>
2014-11-04 09:18:21 -0800

    spinner issue 7

--- src/static/scripts/apps/main-app.js
+++ src/static/scripts/apps/main-app.js
@@ -47,10 +47,34 @@ App.prototype = _.extend(ContourBaseApp.prototype, {
         $('#loading-indicator').show(200);
         // set display:block on the backdrop
         $('#loading-backdrop').addClass('show1');
+        this.spinner('loading-indicator');
         // animate in the backdrop
         setTimeout(function () {
             $('#loading-backdrop').addClass('show2');
         }, 1);
+    },
+
+    spinner: function (id) {
+        var opts = {
+            lines: 8, // The number of lines to draw
+            length: 3, // The length of each line
+            width: 1, // The line thickness
+            radius: 4, // The radius of the inner circle
+            corners: 1, // Corner roundness (0..1)
+            rotate: 0, // The rotation offset
+            direction: 1, // 1: clockwise, -1: counterclockwise
+            color: '#FAFAFA', // #rgb or #rrggbb or array of colors
+            speed: 1, // Rounds per second
+            trail: 60, // Afterglow percentage
+            shadow: false, // Whether to render a shadow
+            hwaccel: false, // Whether to use hardware acceleration
+            className: 'spinner', // The CSS class to assign to the spinner
+            zIndex: 2e9, // The z-index (defaults to 2000000000)
+            top: '50%', // Top position relative to parent
+            left: '80%' // Left position relative to parent
+        };
+        var target = document.getElementById(id);
+        var spinner = new Spinner(opts).spin(target);
     },
 
     hideLoading: function () {
